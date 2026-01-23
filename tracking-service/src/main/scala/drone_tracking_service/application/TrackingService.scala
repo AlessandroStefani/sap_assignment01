940e10b3d1d08c8a776ac4e19287fe258dfb370d
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2026-01-23 16:25:34 +0100

    Update TrackingService.scala

--- tracking-service/src/main/scala/drone_tracking_service/application/TrackingService.scala
+++ tracking-service/src/main/scala/drone_tracking_service/application/TrackingService.scala
@@ -1,7 +1,9 @@
 package drone_tracking_service.application
 
 import cats.effect.IO
+import common.exagonal.InBoundPort
 import drone_tracking_service.domain.{DroneTelemetry, TrackingRequest}
 
+@InBoundPort
 trait TrackingService:
   def trackDrone(request: TrackingRequest): IO[DroneTelemetry]
