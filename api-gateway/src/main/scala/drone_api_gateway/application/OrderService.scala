276a239b3b062206d54d720ad042e17239a2f574
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2026-01-23 17:27:06 +0100

    add tags

--- api-gateway/src/main/scala/drone_api_gateway/application/OrderService.scala
+++ api-gateway/src/main/scala/drone_api_gateway/application/OrderService.scala
@@ -1,10 +1,12 @@
 package drone_api_gateway.application
 
 import cats.effect.IO
+import common.exagonal.OutBoundPort
 import drone_api_gateway.domain.Order
 
 import java.time.Instant
 
+@OutBoundPort
 trait OrderService:
   def placeOrder(userId: String, origin: String, destination: String, weight: Double, departureDate: Instant): IO[String]
 
--- tracking-service/src/main/scala/drone_tracking_service/application/TrackingService.scala
+++ tracking-service/src/main/scala/drone_tracking_service/application/TrackingService.scala
@@ -2,6 +2,8 @@ package drone_tracking_service.application
 
 import cats.effect.IO
 import drone_tracking_service.domain.{DroneTelemetry, TrackingRequest}
+import common.exagonal.InBoundPort
 
+@InBoundPort
 trait TrackingService:
   def trackDrone(request: TrackingRequest): IO[DroneTelemetry]
